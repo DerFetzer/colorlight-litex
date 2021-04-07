cb8804c16231922d6dfd41e5e100082f28c3cec8
SingularitySurfer <norman_krackow@gmx.de>
2021-04-07 09:49:24 +0200

    Accedently pushed to master.. Revert "1. For the colorlight5A-75b V7 there needs to be 0 tx delay, otherwise the PHY doesnt work. 2. The peripheral code has to be inserted at the end of the peripherals list. For me it got inserted at the end after vendorExtentions and this didn't work."
    This reverts commit 0dc8be337c45411e5589e703cc2d897dba88f4ce.

--- soc/colorlight_5a_75x.py
+++ soc/colorlight_5a_75x.py
@@ -255,15 +255,9 @@ class BaseSoC(SoCCore):
 
         # Ethernet / Etherbone ---------------------------------------------------------------------
         if with_ethernet or with_etherbone:
-            if board == "5a-75b" and revision == "7.0":
-                self.submodules.ethphy = LiteEthPHYRGMII(
-                    clock_pads = self.platform.request("eth_clocks", eth_phy),
-                    pads       = self.platform.request("eth", eth_phy),
-                    tx_delay   = 0e-9)
-            else:
-                self.submodules.ethphy = LiteEthPHYRGMII(
-                    clock_pads = self.platform.request("eth_clocks", eth_phy),
-                    pads       = self.platform.request("eth", eth_phy))
+            self.submodules.ethphy = LiteEthPHYRGMII(
+                clock_pads = self.platform.request("eth_clocks", eth_phy),
+                pads       = self.platform.request("eth", eth_phy))
             self.add_csr("ethphy")
             if with_ethernet:
                 self.add_ethernet(phy=self.ethphy)
@@ -295,14 +289,6 @@ class BaseSoC(SoCCore):
 def modify_svd(builder_kwargs):
     # Add Ethernet buffer peripheral to svd
     with open(builder_kwargs["csr_svd"], "r") as f:
-        line_number = 0
-        for l in f:         # search for the right place to insert
-            line_number += 1
-            if """</peripherals>""" in l:
-                line = line_number
-        print("inserting before line:")
-        print(line)
-        f.seek(0)
         s = f.readlines()
     registers = """        <peripheral>
             <name>ETHMEM</name>
@@ -389,7 +375,7 @@ def modify_svd(builder_kwargs):
             </addressBlock>
         </peripheral>
     """
-    s.insert(line-1, registers)
+    s.insert(-2, registers)
     with open(builder_kwargs["csr_svd"], "w") as f:
         f.writelines(s)
 
